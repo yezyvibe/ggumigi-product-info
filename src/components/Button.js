c999125fd70cd31af02ca7985691798c8782ca85
yezyvibe <yeji0501@gmail.com>
2022-02-01 18:58:13 +0900

    feat: 버튼 컴포넌트 생성 및 선택된 상품의 툴팁 출력

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+function Button({ pointX, pointY, isSelected, ...rest }) {
+  return (
+    <>
+      <StyledButton {...rest} pointX={pointX} pointY={pointY}>
+        {isSelected ? "X" : "🔍"}
+      </StyledButton>
+    </>
+  );
+}
+
+export default Button;
+
+const StyledButton = styled.button`
+  border-radius: 50%;
+  background-color: pink;
+  position: absolute;
+  left: ${(props) => `${props.pointX}px`};
+  top: ${(props) => `${props.pointY}px`};
+  cursor: pointer;
+`;
--- src/pages/ProductInfoPage.js
+++ src/pages/ProductInfoPage.js
@@ -1,42 +1,66 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { getProductInfo } from "../utils/api";
 import Tooltip from "../components/Tooltip";
+import Button from "../components/Button";
 
 function ProductInfoPage() {
   const [mainImage, setMainImage] = useState("");
   const [productInfo, setProductInfo] = useState([]);
+  const [selectedProduct, setSelectedProduct] = useState("");
   const getData = async () => {
     try {
       const response = await getProductInfo();
       const { id, imageUrl, productList } = response;
       setMainImage(imageUrl);
-      setProductInfo(productList);
+      addShowState(productList);
     } catch (e) {
       console.error(e);
     }
+  };
+
+  const addShowState = (productList) => {
+    setProductInfo(productList.map((item) => ({ ...item, isChecked: true })));
   };
 
   useEffect(() => {
     getData();
   }, []);
 
-  // useEffect(() => {
-  //   console.log(productInfo);
-  // }, [productInfo]);
+  const onClick = (id) => setSelectedProduct(id);
 
+  useEffect(() => console.log(productInfo), [productInfo]);
+
+  // const onToggleChecked = (id) => {
+  //   setProductInfo((prev) =>
+  //     prev.map((item) =>
+  //       item.id === id ? { ...item, isChecked: !item.isChecked } : item
+  //     )
+  //   );
+  // };
   return (
     <>
-      <PhotoInfoBox src={mainImage}></PhotoInfoBox>
-      {productInfo &&
-        productInfo.map((item) => (
-          <Tooltip
-            productImage={item.imageUrl}
-            productName={item.productName}
-            productPrice={item.priceOriginal}
-            key={`tooltip_list_${item.productId}`}
-          />
+      <Box>
+        <PhotoInfoBox src={mainImage}></PhotoInfoBox>
+        {productInfo.map((item) => (
+          <React.Fragment key={`tooltip_list_${item.productId}`}>
+            <Button
+              onClick={() => onClick(item.productId)}
+              pointX={item.pointX}
+              pointY={item.pointY}
+              isSelected={item.productId === selectedProduct ? true : false}
+            />
+            {item.productId === selectedProduct && (
+              <Tooltip
+                productImage={item.imageUrl}
+                productName={item.productName}
+                productPrice={item.priceOriginal}
+                isSelected={true}
+              />
+            )}
+          </React.Fragment>
         ))}
+      </Box>
     </>
   );
 }
@@ -44,5 +68,11 @@ function ProductInfoPage() {
 export default ProductInfoPage;
 
 const PhotoInfoBox = styled.img`
-  width: 500px;
+  width: 800px;
+  height: 998px;
+`;
+
+const Box = styled.div`
+  width: 1000px;
+  height: 1248px;
 `;
